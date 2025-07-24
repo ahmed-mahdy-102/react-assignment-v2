e649ab35741ab56455817ac3e09973d8af51cd85
Ahmed Mahdy <a.mahdysa3ka@gmail.com>
2025-07-24 21:36:44 +0300

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import Header from './assets/components/pages/Header';
+import About from './assets/components/pages/About';
+import Portfolio from './assets/components/pages/Portfolio';
+import Contact from './assets/components/pages/Contact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Header />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
   </BrowserRouter>
 );
